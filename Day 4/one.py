9915d475eb3ffdbc290f703d9ab762e3793a6292
Synedh <contact@synedh.fr>
2019-12-04 10:48:37 +0100

    Day 3.

--- Day 4/one.py
+++ Day 4/one.py
@@ -1,20 +1,12 @@
-min = 357253
-max = 892942
-
 def check_baguette(v: str):
     dbl = False
     d = 0
     for i in v:
-        for j in v:
-            if i == j:
-                dbl = True
         if int(i) >= d:
             d = int(i)
+            dbl = dbl or v.count(i) >= 2
         else:
             return False
     return dbl
 
-i = 0
-for v in range(min, max + 1):
-    i += int(check_baguette(str(v)))
-print(i)
+print(sum([int(check_baguette(str(v))) for v in range(357253, 892942 + 1)]))

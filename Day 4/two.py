3df5202e9b5c7a7509d41745abf07efa5c923f70
Synedh <contact@synedh.fr>
2019-12-06 13:01:33 +0100

    Day 4.2

---
+++ Day 4/two.py
@@ -0,0 +1,16 @@
+def check_baguette(v: str):
+    dbl = False
+    d = 0
+
+    return ''.join(sorted(v)) == v and len(set(v)) + 1 == len(v)
+        print(v)
+        return True
+    for i in v:
+        if int(i) >= d:
+            d = int(i)
+            dbl = dbl or v.count(i) == 2
+        else:
+            return False
+    return dbl
+
+print(sum([int(check_baguette(str(v))) for v in range(357253, 892942 + 1)]))
